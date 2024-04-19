c02e5193a27701248e24815469410035b8851fc1
nailraskaF <167359252+nailraskaF@users.noreply.github.com>
2024-04-19 13:59:08 -0600

    Add files via upload

---
+++ Etapa 1/8_Aksarlian_EsMayor/8_Aksarlian_EsMayor/8_Aksarlian_EsMayor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Aksarlian_EsMayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            Console.WriteLine("escribe tu edad");
+            edad = int.Parse(Console.ReadLine());
+            if (edad < 18)
+                Console.WriteLine("sos menor papu");
+            else if (edad >= 18)
+                Console.WriteLine("ya sos mayorcito");
+            Console.ReadKey();
+        }
+    }
+}
