6267b1c259833fc6519c8bbab5af1dd33073271b
nailraskaF <167359252+nailraskaF@users.noreply.github.com>
2024-04-19 13:46:00 -0600

    Add files via upload

---
+++ Etapa 1/2_Aksarlian_BienvenidoACiudad/2_Aksarlian_BienvenidoACiudad/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Aksarlian_BienvenidoACiudad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string ciudad;
+            Console.WriteLine("como es tu nombre?");
+            nombre = Console.ReadLine();
+            Console.WriteLine("decime una ciudad");
+            ciudad = Console.ReadLine();
+            Console.WriteLine("hola " + nombre + " bienvenido a " + ciudad);
+
+            Console.ReadKey();
+        }
+    }
+}
