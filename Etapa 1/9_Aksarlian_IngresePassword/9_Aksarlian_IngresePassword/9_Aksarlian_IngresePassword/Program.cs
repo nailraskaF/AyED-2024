42c28797bcb7feb249cdddb8fb60e6f11dab284c
nailraskaF <167359252+nailraskaF@users.noreply.github.com>
2024-05-29 14:29:53 -0300

    Add files via upload

---
+++ Etapa 1/9_Aksarlian_IngresePassword/9_Aksarlian_IngresePassword/9_Aksarlian_IngresePassword/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Aksarlian_IngresePassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contraseña;
+            string intento;
+            contraseña = "pablito";
+            Console.WriteLine("ingrese la contraseña");
+            intento = Console.ReadLine();
+            if (intento == contraseña)
+            {
+                Console.WriteLine("contraseña correcta");
+            }
+            else
+                Console.WriteLine("la contraseña es incorrecta");
+            Console.ReadKey();
+        }
+    }
+}
