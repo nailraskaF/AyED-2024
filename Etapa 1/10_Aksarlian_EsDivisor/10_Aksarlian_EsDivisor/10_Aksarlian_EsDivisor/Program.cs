31631512417dae625fd42fe5012adfd3200495bb
nailraskaF <167359252+nailraskaF@users.noreply.github.com>
2024-05-29 14:32:37 -0300

    Add files via upload

---
+++ Etapa 1/10_Aksarlian_EsDivisor/10_Aksarlian_EsDivisor/10_Aksarlian_EsDivisor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Aksarlian_EsDivisor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1;
+            double num2;
+            Console.WriteLine("escriba un numero q quiera dividir por otro");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("escribe el numero x el que quiera dividir el otro");
+            num2 = int.Parse(Console.ReadLine());
+            if (num1 == 0) {
+                Console.WriteLine("se ha producido un error");
+            }
+            else {
+                Console.WriteLine("el resultado es " + num1 / num2);
+            }
+            Console.ReadKey();
+                
+        }
+    }
+}
