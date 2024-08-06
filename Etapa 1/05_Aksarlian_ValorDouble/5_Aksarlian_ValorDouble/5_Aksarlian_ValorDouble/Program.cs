ba9e0837282f39e7e142d0b0e8201fc5a1607afa
nailraskaF <167359252+nailraskaF@users.noreply.github.com>
2024-08-06 16:38:12 -0300

    Add files via upload

---
+++ Etapa 1/05_Aksarlian_ValorDouble/5_Aksarlian_ValorDouble/5_Aksarlian_ValorDouble/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Aksarlian_ValorDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ancho;
+            double alto;
+            double diagonal;
+
+            Console.WriteLine("escriba un ancho para el rectangulo");
+            ancho = int.Parse(Console.ReadLine());
+            Console.WriteLine("escriba un alto para el rectangulo");
+            alto = int.Parse(Console.ReadLine());
+            Console.WriteLine("el perimetro del rectangulo es " + ((ancho * 2) + (alto * 2)));
+            Console.WriteLine("el area del rectangulo es " + (ancho * alto));
+            Console.WriteLine("la diagonal del rectangulo es " + (Math.Sqrt((ancho * ancho) + (alto * alto))));
+            Console.ReadKey();
+        }
+    }
+}
