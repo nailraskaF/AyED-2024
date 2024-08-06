cd5f6a2d289da5ab1bb03029c7fc5a00ed705151
nailraskaF <167359252+nailraskaF@users.noreply.github.com>
2024-08-06 16:35:48 -0300

    Add files via upload

---
+++ Etapa 1/00_Aksarlian_CalculadoraBasica/0_Aksarlian_CalculadoraBasica/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Aksarlian_CalculadoraBasica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            Console.WriteLine("escribe un numero");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("escribe otro numero");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1 + "+" + num2 + "=" + (num1 + num2));
+            Console.WriteLine(num1 + "-" + num2 + "=" + (num1 - num2));
+            Console.WriteLine(num1 + "*" + num2 + "=" + (num1 * num2));
+            Console.WriteLine(num1 + "/" + num2 + "=" + (num1 / num2));
+            Console.WriteLine("el resto de " + num1 + "/" + num2 + " es igual a " + (num1 % num2));
+
+            Console.ReadKey();
+        }
+    }
+}
