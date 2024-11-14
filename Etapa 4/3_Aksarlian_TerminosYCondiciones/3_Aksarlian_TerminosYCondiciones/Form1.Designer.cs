8d03deaca85197ab64a033719c78b7f77d190503
nailraskaF <167359252+nailraskaF@users.noreply.github.com>
2024-11-14 19:49:56 -0300

    Add files via upload

---
+++ Etapa 4/3_Aksarlian_TerminosYCondiciones/3_Aksarlian_TerminosYCondiciones/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace _3_Aksarlian_TerminosYCondiciones
+{
+    partial class pantalla
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(150, 321);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(330, 20);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "He Leído Y Acepto Términos Y Condiciones";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // pantalla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.ControlBox = false;
+            this.Controls.Add(this.checkBox1);
+            this.MaximizeBox = false;
+            this.Name = "pantalla";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Etapa 4/4_Valdez_CreadorPersonajeRol/4_Valdez_CreadorPersonajeRol/Form2.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace _4_Valdez_CreadorPersonajeRol
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RAZA = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.VIDA = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.HAB_ESP = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Datos Del PJ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "nombre:";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(24, 84);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(13, 13);
+            this.name.TabIndex = 2;
+            this.name.Text = "a";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "raza";
+            // 
+            // RAZA
+            // 
+            this.RAZA.AutoSize = true;
+            this.RAZA.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RAZA.Location = new System.Drawing.Point(24, 120);
+            this.RAZA.Name = "RAZA";
+            this.RAZA.Size = new System.Drawing.Size(13, 13);
+            this.RAZA.TabIndex = 4;
+            this.RAZA.Text = "a";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Vida";
+            // 
+            // VIDA
+            // 
+            this.VIDA.AutoSize = true;
+            this.VIDA.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VIDA.Location = new System.Drawing.Point(24, 156);
+            this.VIDA.Name = "VIDA";
+            this.VIDA.Size = new System.Drawing.Size(13, 13);
+            this.VIDA.TabIndex = 6;
+            this.VIDA.Text = "a";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "habilidades especiales";
+            // 
+            // HAB_ESP
+            // 
+            this.HAB_ESP.AutoSize = true;
+            this.HAB_ESP.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HAB_ESP.Location = new System.Drawing.Point(24, 192);
+            this.HAB_ESP.Name = "HAB_ESP";
+            this.HAB_ESP.Size = new System.Drawing.Size(13, 13);
+            this.HAB_ESP.TabIndex = 8;
+            this.HAB_ESP.Text = "a";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.HAB_ESP);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.VIDA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.RAZA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label RAZA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label VIDA;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label HAB_ESP;
+    }
+}
