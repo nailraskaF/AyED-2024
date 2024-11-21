3becaa0f7a97f5fcd8043439b9a66270c07e0d8f
nailraskaF <167359252+nailraskaF@users.noreply.github.com>
2024-11-21 15:13:47 -0300

    Add files via upload

---
+++ Etapa2/11_Aksarlian_sumandoMatrices/11_Aksarlian_sumandoMatrices/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace _11_Aksarlian_SumandoMatrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+
+
+            Console.WriteLine("columnas");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("filas");
+            m = int.Parse(Console.ReadLine());
+
+
+            int[,] matriz = new int[n, m];
+
+            int[,] zirtam = new int[n, m];
+
+            int[,] resultado = new int[n, m];
+
+            Random rng = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matriz[i, j] = rng.Next(1, 10);
+                    zirtam[i, j] = rng.Next(1, 10);
+                }
+            }
+            Console.WriteLine("-------matriz 1-------");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+
+                    Console.Write(matriz[i, j] + "\t");
+
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("-------2da matriz-------");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+
+                    Console.Write(zirtam[i, j] + "\t");
+
+                }
+                Console.WriteLine("");
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    resultado[i, j] = zirtam[i, j] + matriz[i, j];
+                }
+            }
+            Console.WriteLine("-------suma de las dos anteriores-------");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(resultado[i, j] + "\t");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
